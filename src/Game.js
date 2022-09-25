a7433594024db759e414b8aa5346594e4ca6d9de
LautaroGuardo <lautaro.guardo.99@gmail.com>
2022-09-25 19:55:03 -0300

    agrego botonoes del menu para todas las paginas, creo pagina de error de direccion, creo pagina más juegos. Lautaro Guardo

--- src/Game.js
+++ src/Game.js
@@ -1,8 +1,20 @@
+import Boton from "./Componentes/Boton"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Masjuegos from "./Componentes/MasJuegos";
+import NotFound from "./Componentes/NotFound";
+
 export default function Game()
 {
     return(
         <>
-        <h1>hola</h1>
+        <Boton></Boton>
+        <BrowserRouter>
+        <Routes>
+            <Route path="/masjuegos" element={<Masjuegos/>} />;
+            <Route path="*" element={<NotFound/>} />;
+        </Routes>
+        </BrowserRouter>
         </>
         
     )
